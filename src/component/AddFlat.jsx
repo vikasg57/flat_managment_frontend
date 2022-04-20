0a27b8b72fbeda1306ded14004f8d6a1e28157be
Vikas Gaikwad <vgaikwad299@gmail.com>
2022-04-20 09:08:35 +0530

    upadted

--- src/component/AddFlat.jsx
+++ src/component/AddFlat.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+
 
 import { Link, useNavigate } from "react-router-dom";
 export const AddFlat = () => {
